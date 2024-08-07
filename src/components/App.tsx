aa9cdc37701fe47289e064e6990990f1ef43f58c
wjthompso <49210425+wjthompso@users.noreply.github.com>
2024-08-07 10:36:03 -0700

    Changed folder name

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import { StateNames } from "data/StateNameToAbbrevsMap";
 import { useState } from "react";
 import "../index.css";
 import SelectedMetricIdObject from "../types/componentStatetypes";
-import Header from "./header/Header";
+import Header from "./Header/Header";
 import LeftSidebar from "./LeftSidebar/LeftSidebar";
 import MapArea from "./MapArea/MapArea";
 import RightSidebar from "./RightSidebar";
